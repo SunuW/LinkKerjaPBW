18fc4949500d5234c9b0bca589f7bb177cf548f4
Sandya Yudha <yudhasandya96@gmail.com>
2026-09-17 00:07:21 +0700

    feat(db): tambah tabel users

--- database/schema.sql
+++ database/schema.sql
@@ -1,2 +1,11 @@
 CREATE DATABASE IF NOT EXISTS linkkerja; 
 USE linkkerja; 
+CREATE TABLE users (    
+     id_user       INT AUTO_INCREMENT PRIMARY KEY,     
+     nama          VARCHAR(100) NOT NULL,     
+     email         VARCHAR(150) NOT NULL UNIQUE,    
+     password_hash VARCHAR(255) NOT NULL,     
+     role         ENUM('pencari_kerja', 'perekrut') NOT NULL,     
+     is_verified   BOOLEAN NOT NULL DEFAULT FALSE,     
+     dibuat_pada   TIMESTAMP DEFAULT CURRENT_TIMESTAMP 
+);   
